b92b0de0533da37150a519f2e7694e72a4601f15
danielpenagos <danielpenagos@gmail.com>
2020-08-14 16:16:14 -0500

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/u
 
 sudo apt-get -y install docker-ce
 
-sudo usermod -a -G docker pocaks
+sudo usermod -a -G docker $USER
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
