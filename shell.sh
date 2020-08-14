9950591f6fd0153cf73ad8ff427eb9e63b26683e
danielpenagos <danielpenagos@gmail.com>
2020-08-14 14:26:20 -0500

    archivo bash con prerrequisitos de instalación

---
+++ shell.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get -y install     apt-transport-https ca-certificates     curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+
+sudo apt-get -y install docker-ce
+
+sudo usermod -a -G docker pocaks
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+. ~/.profile
+. ~/.bashrc
+
+nvm install 11
+
+sudo apt -y install openjdk-8-jre-headless
